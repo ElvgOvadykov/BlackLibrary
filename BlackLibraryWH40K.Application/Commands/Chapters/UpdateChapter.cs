48bc69179ff0ef8bed744c64f63c370d37a5110a
Elvg Ovadykov <oelvg@mail.ru>
2021-02-14 23:12:18 +0300

    Implemented the command handler for updating information about the chapter

---
+++ BlackLibraryWH40K.Application/Commands/Chapters/UpdateChapter.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+
+namespace BlackLibraryWH40K.Application.Commands.Chapters
+{
+    /// <summary>
+    /// Команда обновления информации о ордене космодесанта
+    /// </summary>
+    public class UpdateChapter : IRequest
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="сhapter"></param>
+        public UpdateChapter(Store.Model.Chapter сhapter)
+        {
+            Chapter = сhapter;
+        }
+
+        /// <summary>
+        /// Орден
+        /// </summary>
+        public Store.Model.Chapter Chapter { get; set; }
+
+        /// <summary>
+        /// Фабричный метод
+        /// </summary>
+        /// <param name="сhapter"></param>
+        /// <returns></returns>
+        public static UpdateChapter Of(Store.Model.Chapter сhapter) => new UpdateChapter(сhapter);
+    }
+}
---
+++ BlackLibraryWH40K.Application/Commands/Chapters/UpdateChapterHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BlackLibraryWH40K.Helper.Exceptions;
+using BlackLibraryWH40K.Store;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackLibraryWH40K.Application.Commands.Chapters
+{
+    /// <summary>
+    /// Обработчик команды обновления информации о ордене космодесанта
+    /// </summary>
+    public class UpdateChapterHandler : AsyncRequestHandler<UpdateChapter>
+    {
+        private readonly BlackLibraryContext _context;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="context"></param>
+        public UpdateChapterHandler(BlackLibraryContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Реализация команды
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override async Task Handle(UpdateChapter request, CancellationToken cancellationToken)
+        {
+            if (!await _context.Person.AnyAsync(s => s.Id == request.Chapter.ChapterMaster.Id, cancellationToken))
+                throw new NotFoundException("Не удалось обновить информацию о магистре ордена, так как данный персонаж не был найден!");
+
+            if (!await _context.Legion.AnyAsync(s => s.Number == request.Chapter.Legion.Number, cancellationToken))
+                throw new NotFoundException("Не удалось обновить информацию о легионе, так как данный легион не был найден!");
+
+            if (!await _context.World.AnyAsync(s => s.Id == request.Chapter.HomeWorld.Id, cancellationToken))
+                throw new NotFoundException("Не удалось обновить информацию о примархе, так как данный мир не был найден!");
+
+            _context.Chapter.Update(request.Chapter);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
